ba221ff14c9446b69301f6c0cf395b246913260e
Manoj <manojm.developer@gmail.com>
2022-08-18 23:58:59 +0530

    Adding app components and apis

--- server/index.js
+++ server/index.js
@@ -1,17 +1,24 @@
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-const port = 3000;
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
-app.use(express.static('static'));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('pages/index.html'));
-});
+app.use('/posts', postRoutes);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+const CONNECTION_URL = 'mongodb+srv://manojm:mm123456@cluster0.exxawko.mongodb.net/memories_app?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+//mongoose.set('useFindAndModify', false);
